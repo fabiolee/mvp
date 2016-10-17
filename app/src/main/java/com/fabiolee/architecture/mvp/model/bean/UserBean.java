fa8778ad1ec570f151b469f335209af86f79784c
fabiolee <fabiolee59@gmail.com>
2016-10-17 22:46:20 +0800

    - Add realm.

--- app/src/main/java/com/fabiolee/architecture/mvp/model/bean/UserBean.java
+++ app/src/main/java/com/fabiolee/architecture/mvp/model/bean/UserBean.java
@@ -1,9 +1,12 @@
 package com.fabiolee.architecture.mvp.model.bean;
 
+import io.realm.RealmModel;
+
 /**
  * @author fabiolee
  */
-public class UserBean {
+public class UserBean implements RealmModel {
     public String login;
+    public int id;
     public String avatarUrl;
 }
